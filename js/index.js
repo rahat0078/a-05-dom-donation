4c2cdc8c5a10a1c0cd5d7c59293bdf3354f847e6
rahat0078 <rarahat1710@gmail.com>
2024-09-23 02:43:58 +0600

    noakhali donation functionality done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('done')
+
+
+document.getElementById("btn-donate").addEventListener('click', function () {
+    const donationAmount = parseFloat(document.getElementById('donation-amount').value);
+    const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
+    if (isNaN(donationAmount) || donationAmount <= 0) {
+        alert('Invalid Input')
+        return
+    }
+    if(donationAmount> mainBalance){
+        alert('Insufficient Balance')
+        return
+    }
+    const totalDonationEl = parseFloat(document.getElementById('total-donation').innerText)
+
+    const totalDonation = totalDonationEl + donationAmount;
+    
+    document.getElementById('total-donation').innerText = totalDonation
+    
+    
+    const currentBalance = mainBalance - donationAmount;
+    document.getElementById('main-balance').innerText = currentBalance
+})
