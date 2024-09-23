1c88314fb01a1011481456aa8c6dc6984ca21c03
rahat0078 <rarahat1710@gmail.com>
2024-09-23 14:36:17 +0600

    donation and history tab switching done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 /* common function for add donation */
 function addDonation(clickBtn, inputDonation, totalDonationId, mainBalanceId) {
     document.getElementById(clickBtn)
-        .addEventListener('click', function(){
+        .addEventListener('click', function () {
             const inputDonationAmount = getValueById(inputDonation)
             const totalDonationAmount = getInnerTextById(totalDonationId)
             const mainBalanceAmount = getInnerTextById(mainBalanceId)
-            if(inputDonationAmount > mainBalanceAmount){
+            if (inputDonationAmount > mainBalanceAmount) {
                 alert('insufficient balance')
                 return
             }
-            if (isNaN(inputDonationAmount) || inputDonationAmount<= 0) {
+            if (isNaN(inputDonationAmount) || inputDonationAmount <= 0) {
                 alert('Invalid Input')
                 return
             }
             const totalDonation = totalDonationAmount + inputDonationAmount;
             const mainBalance = mainBalanceAmount - inputDonationAmount;
 
-           document.getElementById(totalDonationId).innerText = totalDonation.toFixed(2);
-           document.getElementById(mainBalanceId).innerText = mainBalance.toFixed(2);
+            document.getElementById(totalDonationId).innerText = totalDonation.toFixed(2);
+            document.getElementById(mainBalanceId).innerText = mainBalance.toFixed(2);
         })
 }
 
@@ -33,24 +33,23 @@ addDonation("btn-donate-2", "donation-feni", "total-donation-feni", "main-balanc
 addDonation("btn-donate-3", "donation-quota", "total-donation-qouta", "main-balance")
 
 
-// document.getElementById("btn-donate").addEventListener('click', function () {
-//     const donationAmount = parseFloat(document.getElementById('donation-amount').value);
-//     const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
-//     if (isNaN(donationAmount) || donationAmount <= 0) {
-//         alert('Invalid Input')
-//         return
-//     }
-//     if (donationAmount > mainBalance) {
-//         alert('Insufficient Balance')
-//         return
-//     }
-//     const totalDonationEl = parseFloat(document.getElementById('total-donation').innerText)
+/* toggle history-tab */
+const historyTab = document.getElementById('history-tab')
+const donationTab = document.getElementById('donation-tab')
 
-//     const totalDonation = totalDonationEl + donationAmount;
-
-//     document.getElementById('total-donation').innerText = totalDonation
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add('bg-primary_btn');
+    donationTab.classList.remove('bg-primary_btn')
+    document.getElementById('history').classList.remove('hidden')
+    document.getElementById('donation-main').classList.add('hidden')
+})
 
 
-//     const currentBalance = mainBalance - donationAmount;
-//     document.getElementById('main-balance').innerText = currentBalance
-// })
+
+/* toggle donation tab */
+donationTab.addEventListener('click', function () {
+    donationTab.classList.add('bg-primary_btn')
+    historyTab.classList.remove('bg-primary_btn')
+    document.getElementById('history').classList.add('hidden')
+    document.getElementById('donation-main').classList.remove('hidden')
+})
